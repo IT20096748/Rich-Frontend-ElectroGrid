489d13255d38347d09c1e3d293d61ee1fb037f53
IT20096748 <it20096748@my.sliit.lk>
2022-05-15 20:51:39 +0530

    ComplaintAPI implemented

---
+++ PAF-Practical-ElectroGrid/src/main/java/com/ComplaintAPI.java
@@ -0,0 +1,92 @@
+/*Complaint Servlet*/
+package com;
+
+import com.Complaint;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ComplaintAPI
+ */
+@WebServlet("/ComplaintAPI")
+public class ComplaintAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	Complaint complaintObj = new Complaint();
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ComplaintAPI() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String output = complaintObj.insertComplaint(request.getParameter("cID"), request.getParameter("type"),
+				request.getParameter("cdesc"));
+		response.getWriter().write(output);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Map paras = getParasMap(request);
+		String output = complaintObj.updateComplaint(paras.get("hidItemIDSave").toString(),
+				paras.get("type").toString(), paras.get("cdesc").toString());
+
+		response.getWriter().write(output);
+	}
+
+	private Map getParasMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			String[] params = queryString.split("&");
+			for (String param : params) {
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+		} catch (Exception e) {
+		}
+		return map;
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Map paras = getParasMap(request);
+		String output = complaintObj.deleteComplaint(paras.get("cID").toString());
+		response.getWriter().write(output);
+
+	}
+
+}
